4aa4ca370d07ee22cd50aeed9d305bdce6922da8
israelrodrigues01 <ciceroisrael428@gmail.com>
2025-02-12 23:13:29 -0300

    feat: subo projeto

--- public/script/JSON.js
+++ public/script/JSON.js
@@ -4,20 +4,49 @@ const servicesContent = document.getElementById('services-content');
 
 let projects = [
     {
-        title: 'Blog',
-        image: 'Blog',
-        technology: "HTML e CSS.",
-        desc: 'Desafio 001',
-        demo: "assets/Desafio 001/index.html",
-        repository: 'https://github.com/israelrodrigues01/desafios_codelandia/tree/main/Desafio%20001',
+        title: 'Agendamento de consultas',
+        image: 'agendamentos-consultas',
+        technology: "Engenharia de software",
+        desc: 'Plataforma de Consultas Remotas: Um sistema inovador que permite a realização de atendimentos e consultas a distância, proporcionando conveniência e eficiência para profissionais e clientes no modelo de home office',
+        repository: 'https://www.figma.com/design/xuq8TE75X8bZeBxoIOrBvP/Prot%C3%B3tipo-de-telas-(eng.-software)?node-id=47-109&p=f',
+        repositoryText: 'Figma'
     },
     {
-        title: 'Jordan',
+        title: 'Agendamento hospitalar',
         image: 'JordanShoes',
-        technology: "HTML e CSS.",
-        desc: 'Desafio 002',
-        demo: "assets/Desafio 002/index.html",
-        repository: 'https://github.com/israelrodrigues01/desafios_codelandia/tree/main/Desafio%20002',
+        technology: "C",
+        desc: 'Consiste em um sistema de geração de atendimentos hospitalares agrupados por dias e horários comerciais (das 8h as 11h, e das 13h as 16h) gerados mediante o tratamento e processamento de entradas previamente especificadas por um arquivo txt',
+        repository: 'https://github.com/Marcondes-Amarante/AED_II-Agendamento_hospitalar',
+    },
+    {
+        title: 'Guess The Number Extreme',
+        image: 'guess',
+        technology: "C",
+        desc: 'Jogo para dois jogadores que tentam adivinhar um ponto no espaço (coordenadas x e y de 4 bits). A cada rodada, um jogador faz um palpite, e o jogo informa se a soma das coordenadas chutadas é maior, menor ou igual à soma do ponto oculto',
+        repository: 'https://github.com/Marcondes-Amarante/circuitos-digitais.git',
+    },
+    {
+        title: 'Saúde em Cada',
+        image: 'saude-em-casa',
+        technology: "Engenharia de Software",
+        desc: 'Já pensou tem ter o profissionais na palma da sua mão? Você pode ter isso com o projeto Saúde em Casa. Onde é possível encontrar profissionais qualificados para contratar',
+        repository: 'https://www.figma.com/proto/efGmHaoY9xYXNXnQlli5FR?node-id=39-56&t=XS9vCHOi1Pnge9Fx-6',
+        repositoryText: 'Figma'
+    },
+    {
+        title: 'App de Organização',
+        image: 'app',
+        technology: "Engenharia de Software",
+        desc: 'App de organização, onde pode salvar tarefas, priorizar, ver status e tags. Você pode salvar de acordo com ambientes, seja faculdade, trabalho, casa...',
+        repository: 'https://www.figma.com/proto/SBFquBSiSg3Li8kXg8Vtol?node-id=12-2&t=XS9vCHOi1Pnge9Fx-6',
+        repositoryText: 'Figma'
+    },
+    {
+        title: 'WorkHub',
+        image: 'workhub',
+        technology: "React, Node.js, PostgreSQL, Docker",
+        desc: 'Nosso objetivo é fornecer uma plataforma simples e eficaz para monitorar tarefas, garantindo o controle completo e a organização de equipes',
+        repository: 'https://github.com/FelipeFerraz4/task-management-system',
     },
 ];
 
@@ -93,8 +122,18 @@ projects.map((item) => {
             <p>${item.desc}</p>
         </div>
         <div class="box-btn flex">
-            <a href="${item.demo}" target="_blank" class="btn" style="--bg: var(--green); --color: #171717;">Demo</a>
-            <a href="${item.repository}" target="_blank" class="btn" style="--bg: var(--gray-400); --color: #F9F9F9;">GitHub</a>
+        `;
+
+    if (item.demo) {
+        html += `
+                <a href="${item.demo}" target="_blank" class="btn" style="--bg: var(--green); --color: #171717;">Demo</a> 
+        `;
+    }
+
+    let text = item.repositoryText ? item.repositoryText : 'GitHub';
+
+    html += `
+            <a href="${item.repository}" target="_blank" class="btn" style="--bg: var(--gray-400); --color: #F9F9F9;">${text}</a>
         </div>
     </div>    
     `;
